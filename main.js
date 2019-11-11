838bf800e3de3112f493a49d9b1bc8f7be0dc25d
jey <jey@jeys-MacBook-Pro.local>
2019-11-11 19:19:49 +0300

    Add looping and events

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 
-var myApp = angular.module("myModule", [])
-
-myApp.controller("myController", ($scope) => {
-    $scope.message = "Angular Js in Action"
-})
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", ($scope) => {
+        const country = {
+            name: "Kenya",
+            capital: "Nairobi",
+            flag: "images/kenya.jpeg"
+        }
+        $scope.country = country
+        $scope.message = "Angular Js in Action"
+    })
+    .controller("techController", ($scope)=>{
+    
+        const technologies = [
+            {name: "C#", likes:0, dislikes: 0},
+            {name: "Java", likes:0, dislikes: 0},
+            {name: "Python", likes:0, dislikes: 0},
+            {name: "C++", likes:0, dislikes: 0},
+            {name: "C", likes:0, dislikes: 0},
+            {name: "Go", likes:0, dislikes: 0}
+        ]
+        $scope.technologies = technologies;
+        $scope.incrementLikes = ((technology) => {
+            technology.likes++
+        });
+        $scope.decrementLikes = ((technology) => {
+            technology.dislikes++
+        });
+    })
